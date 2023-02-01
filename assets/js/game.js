ecc533c0f3f5409ee82fdff47dcf84e0a4075d45
AlexDev2808 <colores.2808.8000@gmail.com>
2023-01-31 19:04:21 -0600

    Canvas Responsive

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,12 +3,20 @@ const canvas = document.querySelector('#game')
 // canvas.getContext('2d') para obtener el contexto para un juego en 2d
 const game = canvas.getContext('2d')
 
+let canvasSize;
+let elementsSize;
+
 // Ejecutar la funcion startGame despues de haber cargado el html, el window
-window.addEventListener('load', startGame)
+window.addEventListener('load', setCanvasSize);
 
-function startGame(){
+// Escuchar cada vez que se haga resize
+window.addEventListener('resize', setCanvasSize);
 
-    let canvasSize;
+/*
+Aqui se ejecuta primero setCanvasSize y dentro de setCanvasSize tambien se llama startGame(), ya que si no se hace, el render de los elementos no se ejecutara
+*/
+
+function setCanvasSize(){
 
     if(window.innerHeight > window.innerWidth){
         canvasSize = window.innerWidth * 0.8;
@@ -19,7 +27,12 @@ function startGame(){
     canvas.setAttribute('width', canvasSize)
     canvas.setAttribute('height', canvasSize)
 
-   const elementsSize = canvasSize / 10;
+   elementsSize = canvasSize / 10;
+
+   startGame()
+}
+
+function startGame(){
 
    game.font = elementsSize + 'px Verdana'
    game.textAlign = 'end';
